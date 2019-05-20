b82774d7bd148ad2dfcff03bfa6c320eef6a7c0b
DevSoopark <sparkm0128@gmail.com>
2019-05-20 13:44:07 +0900

    feature(pages): index, about 페이지 추가

---
+++ to-do-list/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from "next/link";
+import Layout from '../components/Layout';
+
+export default class About extends React.Component {
+  render() {
+    return (
+      <Layout>
+      </Layout>
+    )
+  }
+}
---
+++ to-do-list/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from "next/link";
+import Layout from '../components/Layout';
+import Divider from '@material-ui/core/Divider';
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <Layout>
+      </Layout>
+    )
+  }
+}
